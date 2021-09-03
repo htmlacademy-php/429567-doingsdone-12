f30b0577074d371cb57da3348841ea26065e0338
Andrey <XaJIaC@yandex.ru>
2021-09-03 23:20:19 +0300

    Изменил запросы

--- queries.sql
+++ queries.sql
@@ -1,17 +1,17 @@
 INSERT INTO users (userName, email, password) VALUES ('test1','test1@mail.ru','password1'); /* Добавление пользователя test1*/
 INSERT INTO users (userName, email, password) VALUES ('test2','test2@mail.ru','password2'); /* Добавление пользователя test2*/
-INSERT INTO projects (project, user_id) VALUES ('Входящие', '1');/* Добавление задачи Входящие для юзера test1*/
-INSERT INTO projects (project, user_id) VALUES ('Учеба', '1');/* Добавление задачи Учеба для юзера test1*/
-INSERT INTO projects (project, user_id) VALUES ('Работа', '1'); /* Добавление задачи Работа для юзера test1*/
-INSERT INTO projects (project, user_id) VALUES ('Работа', '2'); /* Добавление задачи Работа для юзера test1*/
-INSERT INTO projects (project, user_id) VALUES ('Домашние дела', '2'); /* Добавление задачи Домашние дела для юзера test2*/
-INSERT INTO projects (project, user_id) VALUES ('Авто', '2'); /* Добавление задачи Авто для юзера test2*/
-INSERT INTO tasks (task, date_end, project_id, user_id) VALUES ('Собеседование в IT компании', '18.12.2021', '3', '1'); /*Добавление задачи для проекта Работа юзеру test1*/
-INSERT INTO tasks (task, date_end, project_id, user_id) VALUES ('Выполнить тестовое задание', '10.09.2021', '3', '2'); /*Добавление задачи для проекта Работа юзеру test2*/
-INSERT INTO tasks (task, date_end, project_id, user_id, status) VALUES ('Сделать задание первого раздела', '10.09.2021', '2', '1', '1'); /*Добавление задачи для проекта Учеба юзеру test1*/
-INSERT INTO tasks (task, date_end, project_id, user_id) VALUES ('Встреча с другом', '10.09.2021', '1', '1'); /*Добавление задачи для проекта Входящие  юзеру test1*/
-INSERT INTO tasks (task, date_end, project_id, user_id) VALUES ('Купить корм для кота', '10.09.2021', '5', '2'); /*Добавление задачи для проекта Домашние дела юзеру test2*/
-INSERT INTO tasks (task, date_end, project_id, user_id) VALUES ('Заказать пиццу', '10.09.2021', '5', '2'); /*Добавление задачи для проекта Домашние дела юзеру test2*/
+INSERT INTO projects (project, user_id) VALUES ('Входящие', (SELECT u.id FROM users u WHERE u.userName = 'test1'));/* Добавление задачи Входящие для юзера test1*/
+INSERT INTO projects (project, user_id) VALUES ('Учеба', (SELECT u.id FROM users u WHERE u.userName = 'test1'));/* Добавление задачи Учеба для юзера test1*/
+INSERT INTO projects (project, user_id) VALUES ('Работа', (SELECT u.id FROM users u WHERE u.userName = 'test1')); /* Добавление задачи Работа для юзера test1*/
+INSERT INTO projects (project, user_id) VALUES ('Работа', (SELECT u.id FROM users u WHERE u.userName = 'test2')); /* Добавление задачи Работа для юзера test1*/
+INSERT INTO projects (project, user_id) VALUES ('Домашние дела', (SELECT u.id FROM users u WHERE u.userName = 'test2')); /* Добавление задачи Домашние дела для юзера test2*/
+INSERT INTO projects (project, user_id) VALUES ('Авто', (SELECT u.id FROM users u WHERE u.userName = 'test2')); /* Добавление задачи Авто для юзера test2*/
+INSERT INTO tasks (task, date_end, project_id, user_id) VALUES ('Собеседование в IT компании', '18.12.2021', (SELECT p.id FROM projects p JOIN users u ON project = 'Работа' AND user_id = (SELECT u.id FROM users u WHERE u.userName = 'test1')), (SELECT u.id FROM users u WHERE u.userName = 'test1')); /*Добавление задачи для проекта Работа юзеру test1*/
+INSERT INTO tasks (task, date_end, project_id, user_id) VALUES ('Выполнить тестовое задание', '10.09.2021', (SELECT p.id FROM projects p JOIN users u ON project = 'Работа' AND user_id = (SELECT u.id FROM users u WHERE u.userName = 'test2')), (SELECT u.id FROM users u WHERE u.userName = 'test2')); /*Добавление задачи для проекта Работа юзеру test2*/
+INSERT INTO tasks (task, date_end, project_id, user_id, status) VALUES ('Сделать задание первого раздела', '10.09.2021', (SELECT p.id FROM projects p JOIN users u ON project = 'Учеба' AND user_id = (SELECT u.id FROM users u WHERE u.userName = 'test1')), (SELECT u.id FROM users u WHERE u.userName = 'test1'), '1'); /*Добавление задачи для проекта Учеба юзеру test1*/
+INSERT INTO tasks (task, date_end, project_id, user_id) VALUES ('Встреча с другом', '10.09.2021', (SELECT p.id FROM projects p JOIN users u ON project = 'Входящие' AND user_id = (SELECT u.id FROM users u WHERE u.userName = 'test1')), (SELECT u.id FROM users u WHERE u.userName = 'test1')); /*Добавление задачи для проекта Входящие  юзеру test1*/
+INSERT INTO tasks (task, date_end, project_id, user_id) VALUES ('Купить корм для кота', '10.09.2021', (SELECT p.id FROM projects p JOIN users u ON project = 'Домашние дела' AND user_id = (SELECT u.id FROM users u WHERE u.userName = 'test2')), (SELECT u.id FROM users u WHERE u.userName = 'test2')); /*Добавление задачи для проекта Домашние дела юзеру test2*/
+INSERT INTO tasks (task, date_end, project_id, user_id) VALUES ('Заказать пиццу', '10.09.2021', (SELECT p.id FROM projects p JOIN users u ON project = 'Домашние дела' AND user_id = (SELECT u.id FROM users u WHERE u.userName = 'test2')), (SELECT u.id FROM users u WHERE u.userName = 'test2')); /*Добавление задачи для проекта Домашние дела юзеру test2*/
 
 SELECT project FROM projects WHERE user_id = '1'; /*Выбрать все проекты которые принадлежат пользователю с ID = 1 из таблицы users*/
 SELECT task FROM tasks WHERE project_id = '1'; /* предполагаю, что в таблице проекты все таки не нужна строка user_id*/
