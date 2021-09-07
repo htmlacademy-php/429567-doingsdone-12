8e861874dac06640307428dfdcecfcd0d1bb5785
Andrey <XaJIaC@yandex.ru>
2021-09-08 00:11:54 +0300

    Выполнил и проверил запросы, осталось добавить на страницу

--- index.php
+++ index.php
@@ -1,4 +1,22 @@
 <?php
+$con = mysqli_connect("localhost", "mysql", "", "doingsdone");
+if ($con == false) {
+    print("Ошибка подключения: " . mysqli_connect_error());
+ }
+ else {
+    $sql = "SELECT project FROM projects WHERE user_id = (SELECT u.id FROM users u WHERE u.userName = 'test1')";
+    $result = mysqli_query($con, $sql);
+    if (!$result) {
+        $error = mysqli_error($con);
+        print("Ошибка MySQL: " . $error);
+    }
+    else {
+        $rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
+        foreach ($rows as $row) {
+            print("Проект: " . $row['project']);
+    }
+ }
+}
 require_once ('helpers.php');
 require_once ('config.php');
 require_once ('functions.php');
@@ -7,4 +25,3 @@ $main = include_template ('main.php', ['array_projects' => $array_projects, 'arr
 $page_content = include_template ('layout.php', ['title' => "Дела в порадке", 'main' => $main]);
 print($page_content);
 ?>
-
