dfc1a00697cd27c0f2e83d4421c9eb7fed1cddf8
XaJIaC@yandex.ru <XaJIaC@yandex.ru>
2021-09-08 15:08:30 +0300

    Добавил условия, осталось перебрать массив

--- index.php
+++ index.php
@@ -11,15 +11,25 @@ if ($con == false) {
         print("Ошибка MySQL: " . $error);
     }
     else {
-        $rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
-        foreach ($rows as $row) {
-            print("Проект: " . $row['project']);
+        $array_projects = mysqli_fetch_all($result, MYSQLI_ASSOC);
+        $sql = "SELECT * FROM tasks WHERE user_id = (SELECT u.id FROM users u WHERE u.userName = 'test1')";
+        $result = mysqli_query($con, $sql);
+        if (!$result) {
+            $error = mysqli_error($con);
+            print("Ошибка MySQL: " . $error);
+        }
+        else {
+            $array_info_task = mysqli_fetch_all($result, MYSQLI_ASSOC);
+        }
+            
     }
- }
 }
 require_once ('helpers.php');
 require_once ('config.php');
 require_once ('functions.php');
+echo "<pre>";
+        var_dump ($array_info_task);
+        echo "</pre>";
 $main = include_template ('main.php', ['array_projects' => $array_projects, 'array_info_task' => $array_info_task, 'show_complete_tasks' => $show_complete_tasks, 'timeleft' => $timeleft]);
 
 $page_content = include_template ('layout.php', ['title' => "Дела в порадке", 'main' => $main]);
